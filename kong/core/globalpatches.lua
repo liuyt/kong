ac7b3ee33538c9c4cf06de54ecc050fd28d59172
Thibault Charbonnier <thibaultcha@me.com>
2016-09-09 17:27:28 -0700

    feat(db) implement lua-cassandra 1.0.0 support
    This makes the DAO functional with lua-cassandra 1.0.0. Only Cassandra
    2.x is still supported (more updates required for 3.x).
    
    We also retrieve the cluster release_version to avoid nodes with
    different major versions, and thus prevent migration errors (due to
    different queries being performed for different major versions)
    
    * move DB implementations to `kong.dao.db`
    * updated rockspec tests to properly evaluate `init` modules (typical
    Lua idiom)
    * Get rid of classic.lua and use our own DB implementation helper
    module, it's pretty cool
    * instanciating a DAO Factory now returns `factory, err`
    * instanciating a Cassandra DB in the CLI triggers a cluster refresh
    (testing the connection and retrieving cluster info at the same time)
    * as long as https://github.com/openresty/resty-cli/pull/12 is not
    included in an OpenResty release, this implements a mocked
    `ngx.shared.DICT` object in `kong.globalpatches`.

--- kong/core/globalpatches.lua
+++ kong/core/globalpatches.lua
@@ -1,43 +1,143 @@
-local meta = require "kong.meta"
-local randomseed = math.randomseed
+do
+  local meta = require "kong.meta"
 
-_G._KONG = {
-  _NAME = meta._NAME,
-  _VERSION = meta._VERSION
-}
+  _G._KONG = {
+    _NAME = meta._NAME,
+    _VERSION = meta._VERSION
+  }
+end
 
-local seed
+do
+  local randomseed = math.randomseed
+  local seed
 
---- Seeds the random generator, use with care.
--- Once - properly - seeded, this method is replaced with a stub
--- one. This is to enforce best-practises for seeding in ngx_lua,
--- and prevents third-party modules from overriding our correct seed
--- (many modules make a wrong usage of `math.randomseed()` by calling
--- it multiple times or do not use unique seed for Nginx workers).
---
--- This patched method will create a unique seed per worker process,
--- using a combination of both time and the worker's pid.
--- luacheck: globals math
-_G.math.randomseed = function()
-  if not seed then
-    -- If we're in runtime nginx, we have multiple workers so we _only_
-    -- accept seeding when in the 'init_worker' phase.
-    -- That is because that phase is the earliest one before the
-    -- workers have a chance to process business logic, and because
-    -- if we'd do that in the 'init' phase, the Lua VM is not forked
-    -- yet and all workers would end-up using the same seed.
-    if not ngx.RESTY_CLI and ngx.get_phase() ~= "init_worker" then
-      error("math.randomseed() must be called in init_worker", 2)
+  --- Seeds the random generator, use with care.
+  -- The uuid.seed() method will create a unique seed per worker
+  -- process, using a combination of both time and the worker's pid.
+  -- We only allow it to be called once to prevent third-party modules
+  -- from overriding our correct seed (many modules make a wrong usage
+  -- of `math.randomseed()` by calling it multiple times or do not use
+  -- unique seed for Nginx workers).
+  -- luacheck: globals math
+  _G.math.randomseed = function()
+    if not seed then
+      -- If we're in runtime nginx, we have multiple workers so we _only_
+      -- accept seeding when in the 'init_worker' phase.
+      -- That is because that phase is the earliest one before the
+      -- workers have a chance to process business logic, and because
+      -- if we'd do that in the 'init' phase, the Lua VM is not forked
+      -- yet and all workers would end-up using the same seed.
+      if not ngx.RESTY_CLI and ngx.get_phase() ~= "init_worker" then
+        error("math.randomseed() must be called in init_worker", 2)
+      end
+
+      seed = ngx.time() + ngx.worker.pid()
+      ngx.log(ngx.DEBUG, "random seed: ", seed, " for worker nb ", ngx.worker.id(),
+                         " (pid: ", ngx.worker.pid(), ")")
+      randomseed(seed)
+    else
+      ngx.log(ngx.DEBUG, "attempt to seed random number generator, but ",
+                         "already seeded with ", seed)
     end
 
-    seed = ngx.time() + ngx.worker.pid()
-    ngx.log(ngx.DEBUG, "random seed: ", seed, " for worker nb ", ngx.worker.id(),
-                       " (pid: ", ngx.worker.pid(), ")")
-    randomseed(seed)
-  else
-    ngx.log(ngx.DEBUG, "attempt to seed random number generator, but ",
-                       "already seeded with ", seed)
+    return seed
   end
+end
 
-  return seed
+if ngx.RESTY_CLI then
+  do
+    -- ngx.shared.DICT proxy
+    -- https://github.com/bsm/fakengx/blob/master/fakengx.lua
+    local SharedDict = {}
+    local function set(data, key, value)
+      data[key] = {
+        value = value,
+        info = {expired = false}
+      }
+    end
+    function SharedDict:new()
+      return setmetatable({data = {}}, {__index = self})
+    end
+    function SharedDict:get(key)
+      return self.data[key] and self.data[key].value, nil
+    end
+    function SharedDict:set(key, value)
+      set(self.data, key, value)
+      return true, nil, false
+    end
+    SharedDict.safe_set = SharedDict.set
+    function SharedDict:add(key, value)
+      if self.data[key] ~= nil then
+        return false, "exists", false
+      end
+      set(self.data, key, value)
+      return true, nil, false
+    end
+    function SharedDict:replace(key, value)
+      if self.data[key] == nil then
+        return false, "not found", false
+      end
+      set(self.data, key, value)
+      return true, nil, false
+    end
+    function SharedDict:delete(key)
+      self.data[key] = nil
+      return true
+    end
+    function SharedDict:incr(key, value)
+      if not self.data[key] then
+        return nil, "not found"
+      elseif type(self.data[key].value) ~= "number" then
+        return nil, "not a number"
+      end
+      self.data[key].value = self.data[key].value + value
+      return self.data[key].value, nil
+    end
+    function SharedDict:flush_all()
+      for _, item in pairs(self.data) do
+        item.info.expired = true
+      end
+    end
+    function SharedDict:flush_expired(n)
+      local data = self.data
+      local flushed = 0
+
+      for key, item in pairs(self.data) do
+        if item.info.expired then
+          data[key] = nil
+          flushed = flushed + 1
+          if n and flushed == n then
+            break
+          end
+        end
+      end
+      self.data = data
+      return flushed
+    end
+    function SharedDict:get_keys(n)
+      n = n or 1024
+      local i = 0
+      local keys = {}
+      for k in pairs(self.data) do
+        keys[#keys+1] = k
+        i = i + 1
+        if n ~= 0 and i == n then
+          break
+        end
+      end
+      return keys
+    end
+
+    -- hack
+    _G.ngx.shared = setmetatable({}, {
+      __index = function(self, key)
+        local shm = rawget(self, key)
+        if not shm then
+          shm = SharedDict:new()
+          rawset(self, key, SharedDict:new())
+        end
+        return shm
+      end
+    })
+  end
 end
